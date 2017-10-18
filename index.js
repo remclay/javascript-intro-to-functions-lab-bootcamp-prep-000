88fc84c5814354fe8222709ced88712ffe66f9e2
Remmy Clay <remmy.lauren@gmail.com>
2017-10-18 22:55:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+  return string
+}
+
+if (sayHiToGrandma(string) === .toLowerCase){
+  console.log("I can't hear you!")
+} else if (sayHiToGrandma(string) === .toUpperCase){
+  console.log("YES INDEED!")
+} else if (sayHiToGrandma(string) === "I love you, Grandma"){
+  console.log("I love you, too")
+}
