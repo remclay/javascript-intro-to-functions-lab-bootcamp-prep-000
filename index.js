ed74bf0d6d7ebb271ed2407c4cec2bcd0e23e42b
Remmy Clay <remmy.lauren@gmail.com>
2017-10-19 00:13:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string){
+var string = function sayHiToGrandma(string){
   if (string === string.toLowerCase){
   return `I can't hear you!`
   }
